8ed989f8e5d2156af8597a39a6f22b7c26a33c14
jmontanos <joss92821@hotmail.com>
2021-08-06 20:56:22 -0500

    Agregando estuctura html y estilos completos, se arreglo la funcionalidad js

--- 5. Calculadora de horas extras/horas-extras.js
+++ 5. Calculadora de horas extras/horas-extras.js
@@ -1,10 +1,3 @@
-// DOM : llamar campos del html
-let salario = document.getElementById("salario");
-let horasDiariasLaborales = document.getElementById("horas-diarias-laborales");
-
-// Constantes
-const VALORDIALABORADA = salario / 30 / horasDiariasLaborales;
-
 const RECARGO = {
   extraDiurno: 1.25,
   extraNocturno: 1.75,
@@ -12,18 +5,55 @@ const RECARGO = {
   extraNocturnoDominical: 2.5,
 };
 
-// Calcular horas extras llegan por parametros
-const calcularHorasExtras = ({ horas, tipoRecargo }) => {
-  const valorTotalHorasExtras = [];
-  if (RECARGO) {
-    const valorHoraExtra = VALORDIALABORADA * RECARGO[tipoRecargo] * horas;
-    valorTotalHorasExtras.push(valorHoraExtra);
-  }
+const calcularHorasExtras = () => {
+  // DOM : llamar campos del html
+  const inputSalario = document.getElementById("input-salario");
+  const inputDiurna = document.getElementById("input-diurna");
+  const inputNocturna = document.getElementById("input-nocturna");
+  const inputDiurnaDominical = document.getElementById(
+    "input-diurna-dominical"
+  );
+  const inputNocturnaDominical = document.getElementById(
+    "input-nocturna-dominical"
+  );
+  // Valores
+  const valueSalario = inputSalario.value;
+  const valueDiurna = inputDiurna.value;
+  const valueNocturna = inputNocturna.value;
+  const valueDiurnaDominical = inputDiurnaDominical.value;
+  const valueNocturnaDominical = inputNocturnaDominical.value;
 
-  return valorTotalHorasExtras.reduce((a, b) => {
-    return a + b;
-  });
+  // Constantes
+  const diasMes = 30;
+  const horasDiariasLaborales = 8;
+  const valorDiaLaborado = valueSalario / diasMes / horasDiariasLaborales;
+  const valorTotalHorasExtras = [];
+
+  // Calcular horas extras
+  const ValorHoraExtraDiurna =
+    valorDiaLaborado * (RECARGO.extraDiurno * parseInt(valueDiurna));
+  const ValorHoraExtraNocturna =
+    valorDiaLaborado * (RECARGO.extraNocturno * parseInt(valueNocturna));
+  const ValorHoraExtraDiurnaDominical =
+    valorDiaLaborado *
+    (RECARGO.extraDiurnoDominical * parseInt(valueDiurnaDominical));
+  const ValorHoraExtraNocturnaDominical =
+    valorDiaLaborado *
+    (RECARGO.extraNocturnoDominical * parseInt(valueNocturnaDominical));
+
+  // Almacenar todos los valores horas extra
+  valorTotalHorasExtras.push(ValorHoraExtraDiurna);
+  valorTotalHorasExtras.push(ValorHoraExtraNocturna);
+  valorTotalHorasExtras.push(ValorHoraExtraDiurnaDominical);
+  valorTotalHorasExtras.push(ValorHoraExtraNocturnaDominical);
+
+  return sumarValorHorasExtras(valorTotalHorasExtras);
 };
 
-// Enviar argumentos
-calcularHorasExtras();
+// Sumatoria Total
+const sumarValorHorasExtras = (valorTotalHorasExtras) => {
+   // DOM : llamar campos del html
+  const inputRespuesta = document.getElementById("respuesta");
+  const total = valorTotalHorasExtras.reduce((a, b) => a + b); 
+  return inputRespuesta.innerHTML = `Total Horas Extras a pagar: ${total.toFixed(2)}`
+};
